02b1bf306324edaae903c91585ac6f277381843a
anujbarve <anujbarve27@gmail.com>
2023-05-07 01:36:53 +0530

    updated db

--- scr.sql
+++ scr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 05, 2023 at 03:20 PM
+-- Generation Time: May 06, 2023 at 10:06 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -54,6 +54,50 @@ INSERT INTO `admins` (`id`, `username`, `password`, `email`, `security_code`, `a
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `assignments`
+--
+
+CREATE TABLE `assignments` (
+  `id` int(11) NOT NULL,
+  `assignment_name` varchar(255) NOT NULL,
+  `assigned_course` int(11) NOT NULL,
+  `assignment_desc` text NOT NULL,
+  `assignment_date` varchar(255) NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `assignments`
+--
+
+INSERT INTO `assignments` (`id`, `assignment_name`, `assigned_course`, `assignment_desc`, `assignment_date`) VALUES
+(1, 'Implement Tree ', 1, 'Implement Tree DS using CPP', '2023-05-07 00:49:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `completed_assignments`
+--
+
+CREATE TABLE `completed_assignments` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `assignment_name` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `source_code` text NOT NULL,
+  `lang` int(11) NOT NULL,
+  `completed_time` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `completed_assignments`
+--
+
+INSERT INTO `completed_assignments` (`id`, `filename`, `assignment_name`, `user`, `source_code`, `lang`, `completed_time`) VALUES
+(1, '', 'Implement Tree ', 'anujbarve', 'asdfsadfsadf', 50, '2023-05-07');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `contact`
 --
 
@@ -92,7 +136,7 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `course_name`, `course_teacher`, `assignments_no`) VALUES
-(2, 'Adv Java', 'Anuj Vinod Barve', 0);
+(1, 'DSA', 'DSA Teacher', NULL);
 
 -- --------------------------------------------------------
 
@@ -113,15 +157,17 @@ CREATE TABLE `teachers` (
   `teacher_in` varchar(255) DEFAULT NULL,
   `teacher_tw` varchar(255) DEFAULT NULL,
   `teacher_desc` text DEFAULT NULL,
-  `teacher_photo` varchar(255) DEFAULT NULL
+  `teacher_photo` varchar(255) DEFAULT NULL,
+  `last_login` bigint(20) DEFAULT NULL,
+  `assigned_course` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `teachers`
 --
 
-INSERT INTO `teachers` (`teacherID`, `teacherName`, `teacherEmail`, `teacherAddr`, `teacherPhone`, `teacherUid`, `teacherPwd`, `teacher_ln`, `teacher_gh`, `teacher_in`, `teacher_tw`, `teacher_desc`, `teacher_photo`) VALUES
-(1, 'Anuj Vinod Barve', 'anujbarve27@gmail.com', '102,bell avenue', '09604345549', 'teacher', '$2y$10$LQMRxn4szY9XCX2theZHwudnMG7b//BbBLC1ikpVxL0xX3NXTs4jq', 'https://in.linkedin.com/in/anuj-barve-838b9b162', 'https://github.com/anujbarve/', 'https://github.com/anujbarve/', ' https://github.com/anujbarve/', 'asfdasdfsd', NULL);
+INSERT INTO `teachers` (`teacherID`, `teacherName`, `teacherEmail`, `teacherAddr`, `teacherPhone`, `teacherUid`, `teacherPwd`, `teacher_ln`, `teacher_gh`, `teacher_in`, `teacher_tw`, `teacher_desc`, `teacher_photo`, `last_login`, `assigned_course`) VALUES
+(1, 'DSA Teacher', 'dsateach@mail.com', '', '', 'dsa', '$2y$10$18JLjtr9BPZobzH.gsNUvenXpmzLrnm5bOL5RaK.knfMMh4Jnd0Zm', '', '', '', '', '', NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -143,21 +189,22 @@ CREATE TABLE `users` (
   `user_tw` varchar(255) DEFAULT NULL,
   `user_desc` text DEFAULT NULL,
   `user_photo` varchar(255) DEFAULT NULL,
-  `last_login` bigint(20) DEFAULT NULL
+  `last_login` bigint(20) DEFAULT NULL,
+  `assigned_course` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `userName`, `userEmail`, `userAddr`, `userPhone`, `userUid`, `userPwd`, `user_ln`, `user_gh`, `user_in`, `user_tw`, `user_desc`, `user_photo`, `last_login`) VALUES
-(1, 'Anuj Vinod Barve', 'anujbarve27@gmail.com', 'asdfasfd', '09604345549', 'anujbarve', '$2y$10$SLU7.nBgkfijYEv3iWIGCO/ObECw9wqwFQciEU8eczhO0QJwU71Zy', 'adfsafd', 'asfdasfd', 'asfasfd', 'asfsadf', 'asdfasfdsadfasfsdfasdfsadfdasfasdfsdafsadf', 'pfp.jpg', 1683292394),
-(2, 'Pratik Rahane', 'pratikrahane@gmail.com', '', '', 'pratikrahane', '$2y$10$qU1SuYDVzyqWxDdbn2ec3eYA3uUOEihZvYR/VZ/trYWORGb4dcb22', '', '', '', '', '', NULL, NULL),
-(3, 'Om Thorat', 'omthorat@gmail.com', NULL, NULL, 'omthorat', '$2y$10$j40GIO1PU.3g7B8uoCWd3OJvErCKGlnZN4TVvI1l/o29nDV.Ez30S', NULL, NULL, NULL, NULL, NULL, NULL, 1683214610),
-(4, 'Dhiraj Yadav', 'dhirajyadav@gmail.com', NULL, NULL, 'dhirajyadav', '$2y$10$/tGEMK1tbNg4CeHjv6Vefe3nH.xgb8FKeJBRCXQnM3J1XgIy8eJZm', NULL, NULL, NULL, NULL, NULL, NULL, 1683214701),
-(5, 'Swarali Surve', 'swaralisurve@gmail.com', NULL, NULL, 'swaralisurve', '$2y$10$TG44Va8HmoBPMMQtRJbLcueRDUv9f5YslZf.uXeVFPwP3/k77QyDy', NULL, NULL, NULL, NULL, NULL, NULL, 1683214836),
-(6, 'Ishwari Yadav', 'ishwariyadav@gmail.com', NULL, NULL, 'ishwariyadav', '$2y$10$SLU7.nBgkfijYEv3iWIGCO/ObECw9wqwFQciEU8eczhO0QJwU71Zy', NULL, NULL, NULL, NULL, NULL, NULL, 1683214934),
-(8, 'dedemo ', 'sdfgsdfg@mail.com', NULL, NULL, 'alex', '$2y$10$Ko0W19z/bB/ZvN/2h7a8EeHwVMSVAV4NBaWoHQgFdyVPi4hFxQgK2', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `users` (`userID`, `userName`, `userEmail`, `userAddr`, `userPhone`, `userUid`, `userPwd`, `user_ln`, `user_gh`, `user_in`, `user_tw`, `user_desc`, `user_photo`, `last_login`, `assigned_course`) VALUES
+(1, 'Anuj Vinod Barve', 'anujbarve27@gmail.com', 'Nasik, Maharashtra, India ', '9604345549', 'anujbarve', '$2y$10$SLU7.nBgkfijYEv3iWIGCO/ObECw9wqwFQciEU8eczhO0QJwU71Zy', 'https://in.linkedin.com/in/anuj-barve-838b9b162', 'https://github.com/anujbarve/', 'https://www.instagram.com/anuj_barve/', 'https://twitter.com/BarveAnuj', 'Web and App Developer | Flutter Enthusiast | Student | Aspiring Computer Engineer | Introvert / Extrovert Depends on the situation ; )', 'pfp.jpg', 1683403587, 1),
+(2, 'Pratik Rahane', 'pratikrahane@gmail.com', '', '', 'pratikrahane', '$2y$10$qU1SuYDVzyqWxDdbn2ec3eYA3uUOEihZvYR/VZ/trYWORGb4dcb22', '', '', '', '', '', NULL, NULL, NULL),
+(3, 'Om Thorat', 'omthorat@gmail.com', NULL, NULL, 'omthorat', '$2y$10$j40GIO1PU.3g7B8uoCWd3OJvErCKGlnZN4TVvI1l/o29nDV.Ez30S', NULL, NULL, NULL, NULL, NULL, NULL, 1683214610, NULL),
+(4, 'Dhiraj Yadav', 'dhirajyadav@gmail.com', NULL, NULL, 'dhirajyadav', '$2y$10$/tGEMK1tbNg4CeHjv6Vefe3nH.xgb8FKeJBRCXQnM3J1XgIy8eJZm', NULL, NULL, NULL, NULL, NULL, NULL, 1683214701, NULL),
+(5, 'Swarali Surve', 'swaralisurve@gmail.com', NULL, NULL, 'swaralisurve', '$2y$10$TG44Va8HmoBPMMQtRJbLcueRDUv9f5YslZf.uXeVFPwP3/k77QyDy', NULL, NULL, NULL, NULL, NULL, NULL, 1683214836, NULL),
+(6, 'Ishwari Yadav', 'ishwariyadav@gmail.com', NULL, NULL, 'ishwariyadav', '$2y$10$SLU7.nBgkfijYEv3iWIGCO/ObECw9wqwFQciEU8eczhO0QJwU71Zy', NULL, NULL, NULL, NULL, NULL, NULL, 1683214934, NULL),
+(8, 'dedemo ', 'sdfgsdfg@mail.com', NULL, NULL, 'alex', '$2y$10$Ko0W19z/bB/ZvN/2h7a8EeHwVMSVAV4NBaWoHQgFdyVPi4hFxQgK2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -171,6 +218,7 @@ CREATE TABLE `user_files` (
   `extension` varchar(255) DEFAULT NULL,
   `source_code` text DEFAULT NULL,
   `user_name` varchar(255) DEFAULT NULL,
+  `assignment_id` int(11) NOT NULL,
   `time_stamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -178,9 +226,9 @@ CREATE TABLE `user_files` (
 -- Dumping data for table `user_files`
 --
 
-INSERT INTO `user_files` (`id`, `name`, `extension`, `source_code`, `user_name`, `time_stamp`) VALUES
-(1, 'Hello World In CPP', '52', '#include <iostream>\n\nusing namespace std;\n\nint main()\n{\n    cout<<\"Hello World\";\n}', 'anujbarve', '2023-05-04 15:56:30'),
-(3, 'Python hello world', '71', 'print(\"Hello world\")', 'anujbarve', '2023-05-04 18:32:24');
+INSERT INTO `user_files` (`id`, `name`, `extension`, `source_code`, `user_name`, `assignment_id`, `time_stamp`) VALUES
+(1, 'Hello World In CPP', '52', '#include <iostream>\n\nusing namespace std;\n\nint main()\n{\n    cout<<\"Hello World\";\n}', 'anujbarve', 0, '2023-05-04 15:56:30'),
+(3, 'Python hello world', '71', 'print(\"Hello world\")', 'anujbarve', 0, '2023-05-04 18:32:24');
 
 -- --------------------------------------------------------
 
@@ -206,6 +254,18 @@ CREATE TABLE `user_posts` (
 -- Indexes for table `admins`
 --
 ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `completed_assignments`
+--
+ALTER TABLE `completed_assignments`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -255,6 +315,18 @@ ALTER TABLE `admins`
   MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `assignments`
+--
+ALTER TABLE `assignments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `completed_assignments`
+--
+ALTER TABLE `completed_assignments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
@@ -264,13 +336,13 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `teacherID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `teacherID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
